53a9f3c729a7be874681676a875cdf3309098822
TomMonks <thomas.m.monks@gmail.com>
2013-03-11 15:54:21 +0000

    Results of dedup are returned to and displayed in interface

--- MainWindow.py
+++ MainWindow.py
@@ -6,7 +6,7 @@ Module implementing MainWindow.
 
 from PyQt4.QtGui import QMainWindow
 from PyQt4.QtCore import pyqtSignature
-from PyQt4.QtGui import QFileDialog
+from PyQt4.QtGui import QFileDialog,  QSizePolicy
 
 from Ui_MainWindow import Ui_MainWindow
 
@@ -22,6 +22,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         """
         QMainWindow.__init__(self, parent)
         self.setupUi(self)
+        self.table_summary.setRowCount(4)
+        self.table_summary.setColumnCount(2)
+        self.table_summary.horizontalHeader().setVisible(True)
+        self.table_summary.horizontalHeader().setVisible(True)
+        
+        #hide the results widget
+        self.results_tab = self.tabWidget.widget(1)
+        self.tabWidget.removeTab(1)
     
     @pyqtSignature("")
     def on_pushButton_clicked(self):
@@ -42,7 +50,28 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     @pyqtSignature("")
     def on_pushButton_2_clicked(self):
         """
-        Slot documentation goes here.
+        Runs the depulication and displays results
         """
-        # TODO: not implemented yet
-        run_dedup(str(self.txt_file.text()))
+        results = run_dedup(str(self.txt_file.text()))
+        self.display_results(results)
+
+    def display_results(self,  results):
+        self.show_record_count(results)
+        self.show_file_paths(self.txt_file.text())
+        
+        self.tabWidget.addTab(self.results_tab,  'Results')
+        self.tabWidget.setCurrentIndex(1)
+        
+    def show_record_count(self,  results):
+        self.table_summary.item(0, 0).setText(str(results.count_found()))
+        self.table_summary.item(1, 0).setText(str(results.count_unique()))
+        self.table_summary.item(2, 0).setText(str(results.count_title_duplicates()))
+        self.table_summary.item(3, 0).setText(str(results.count_likely_duplicates()))
+        
+        
+    def show_file_paths(self,  fname):
+        self.table_summary.item(0, 1).setText(fname)
+        self.table_summary.item(1, 1).setText(fname[:-4] + '_edit.txt')
+        self.table_summary.item(2, 1).setText(fname[:-4] + '_dups.txt')
+        self.table_summary.item(3, 1).setText(fname[:-4] + '_likely_dups.txt')
+        self.table_summary.horizontalHeader().setStretchLastSection(True)
--- dedupMacro.py
+++ dedupMacro.py
@@ -1,4 +1,5 @@
 import string
+
 import sys
 from dedupFuncs import *
 
@@ -20,3 +21,31 @@ def run_dedup(fileName):
     print 'deduplication complete'
     print 'duplicates removed: ', len(edited_records.duplicates)
     print 'Possible duplicates: ', len(likely_dups)
+    
+    return DedupResultContainer(all_records,  edited_records, likely_dups)
+    
+    
+
+
+class DedupResultContainer():
+    def __init__(self,  found,  edited,  likely):
+        """
+        Constructor
+        """
+        self.found = found
+        self.edited_records = edited
+        self.likely_dups = likely
+
+    def count_found(self):
+        return len(self.found)
+        
+    def count_title_duplicates(self):
+        return len(self.edited_records.duplicates)
+        
+    def count_likely_duplicates(self):
+        return len(self.likely_dups)
+            
+    def count_unique(self):
+        return len(self.found) -  len(self.edited_records.duplicates)
+        
+        
